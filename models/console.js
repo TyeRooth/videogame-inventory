7206849db2a584141f33432499442999f12bb817
Tye Rooth <tyerooth@gmail.com>
2022-10-29 10:03:56 -0400

    videogame, console, and genre models created

---
+++ models/console.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ConsoleSchema = new Schema ({
+    name: { type: String, required: true, maxLength: 100 },
+    releaseYear: { type: String, required: true, maxLength: 4 },
+    price: { type: Number, required: true, min: 0 },
+});
+
+ConsoleSchema.virtual("url").get(function () {
+    return `inventory/console/${ this._id }`;
+});
+
+module.exports = mongoose.model("Console", ConsoleSchema);
--- models/videogame.js
+++ models/videogame.js
@@ -2,6 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const videogameSchema = new Schema({
-    
-})
+const VideogameSchema = new Schema({
+    name: { type: String, required: true, maxLength: 100 },
+    price: { type: Number, required: true, min: 0 },
+    stock: { type: Number, required: true, min: 0 },
+    ESRB: { type: String, required: true, enum: ["E", "E10+", "T", "M"], default: "E" },
+    releaseDate: { type: Date },
+    genre: [{ type: Schema.Types.ObjectId, ref: "Genre "}],
+    console: [{ type: Schema.Types.ObjectId, ref: "Console"}],
+});
+
+VideogameSchema.virtual("url").get(function () {
+    return `/inventory/videogame/${ this._id }`;
+});
+
+module.exports = mongoose.model("Videogame", VideogameSchema);
